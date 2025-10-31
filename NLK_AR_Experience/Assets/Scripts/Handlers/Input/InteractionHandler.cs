ce08233c593491a034f52be1d674af26186db822
Emil Nyberg <nyberg.emil01@gmail.com>
2025-10-31 20:44:45 +0100

    One can now select an object

--- NLK_AR_Experience/Assets/Scripts/Handlers/Input/InteractionHandler.cs
+++ NLK_AR_Experience/Assets/Scripts/Handlers/Input/InteractionHandler.cs
@@ -7,12 +7,42 @@ using Logger = NLKARExperience.Util.Logger;
 
 namespace NLKARExperience.Handlers
 {
+    /// <summary>
+    /// Interaction handler that handles the flow of interaction in the app
+    /// </summary>
+    /// <remarks>
+    /// This class utilizes the <see cref="UIInputHandler"/>, <see cref="ObjectSelectionInputHandler"/> and <see cref="ARPlaneTouchToPoseHandler"/>
+    /// to define the interaction flow of the application.
+    /// <para>
+    /// It requires an <see cref="UIInputHandler"/>, <see cref="ObjectSelectionInputHandler"/> and <see cref="ARPlaneTouchToPoseHandler"/> reference to
+    /// work.
+    /// </para>
+    /// </remarks>
     public class InteractionHandler : MonoBehaviour, IUserInputHandler
     {
-        [SerializeField] IUserInputHandler _uiInputHandler;
-        [SerializeField] IUserInputHandler _objectSelectionHandler;
-        [SerializeField] IUserInputHandler _arPlaneToPoseHandler;
+        //Want to add a custom property drawer so that I can use IUserInptuHandler as editor exposed seriealizeables instead
+        /// <summary>
+        /// Cached reference to the <see cref="UIInputHandler"/>
+        /// </summary>
+        [SerializeField] UIInputHandler _uiInputHandler;
 
+        /// <summary>
+        /// Cached reference to the <see cref="ObjectSelectionInputHandler"/>
+        /// </summary>
+        [SerializeField] ObjectSelectionInputHandler _objectSelectionHandler;
+
+        /// <summary>
+        /// Cached reference to the <see cref="ARPlaneTouchToPoseHandler"/>
+        /// </summary>
+        [SerializeField] ARPlaneTouchToPoseHandler _arPlaneToPoseHandler;
+
+        /// <summary>
+        /// Validates that the required dependencies exists/are valid.
+        /// </summary>
+        /// <remarks>
+        /// If one or more reference is invalid, this component will log an error message and disable itself 
+        /// to prevent null reference exceptions.
+        /// </remarks>
         void Start()
         {
             if (_uiInputHandler == null)
@@ -37,13 +67,18 @@ namespace NLKARExperience.Handlers
             }
         }
 
+        /// <summary>
+        /// Controls the interaction flow of the application. 
+        /// </summary>
+        /// <param name="touchPosition">The position of the user's touch ón the screen</param>
+        /// <returns><c>true</c> if a UI elementm selectable object or AR plane is hit, otherwise <c>false</c>.</returns>
         public bool HandleUserTouchedScreen(Vector2 touchPosition)
         {
             if (!enabled) return false;
 
-            if (_uiInputHandler.HandleUserTouchedScreen(touchPosition)) return true;
+            if (_uiInputHandler.HandleUserTouchedScreen(touchPosition))         return true;
             if (_objectSelectionHandler.HandleUserTouchedScreen(touchPosition)) return true;
-            if (_arPlaneToPoseHandler.HandleUserTouchedScreen(touchPosition)) return true;
+            if (_arPlaneToPoseHandler.HandleUserTouchedScreen(touchPosition))   return true;
 
             return false;
         }
